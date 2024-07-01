0b570bbfe817339c133826b7d52a3c4a5285e9dc
Leonardo Pereira <leonardo_human@hotmail.com>
2024-07-01 11:22:44 -0300

    feat: implemented requests mocks

---
+++ src/api-requests/mocks/index.ts
@@ -0,0 +1,40 @@
+import { setupWorker } from 'msw/browser'
+
+import { env } from '../../env'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getOrderDetailsMock } from './get-order-details-mock'
+import { getOrdersMock } from './get-orders-mock'
+import { getPopularProductsMock } from './get-popular-products-mock '
+import { getProfileMock } from './get-profile-mock'
+import { updateProfileMock } from './get-update-profile-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { signInMock } from './sign-in-mock'
+
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getPopularProductsMock,
+  getDailyRevenueInPeriodMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
+  getOrdersMock,
+  getOrderDetailsMock,
+)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+
+  await worker.start()
+}
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -7,7 +7,7 @@ export const api = axios.create({
   withCredentials: true,
 })
 
-if (env.VITE_ENABLE_API_DELAY) {
+if (env.MODE !== 'test' && env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
     await new Promise((resolve) => setTimeout(resolve, Math.random() * 3000))
 
